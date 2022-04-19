f1eec8d8c6998ac8890e262c22ee1811818e5d80
Adrian Villegas <manbassman1996@gmail.com>
2022-04-19 17:18:36 -0300

    otro

--- back-src/index.ts
+++ back-src/index.ts
@@ -1,8 +1,9 @@
 import * as express from "express";
 import * as path from "path";
+import * as cors from "cors";
 import { updateProfile, getProfile } from "./controllers/users-controller";
 import { User } from "./models/user";
-const staticDirPath = path.resolve(__dirname, "../dist");
+const staticDirPath = path.resolve(__dirname, "../dist/index.html");
 const port = process.env.PORT || 3004;
 
 const app = express();
@@ -11,15 +12,16 @@ app.use(
     limit: "50mb",
   })
 );
+app.use(cors());
 app.get("/env", (req, res) => {
   res.json({
-    environment: process.env.NODE_ENV,
+    message: "todo ok",
   });
 });
 
-app.use(express.static(staticDirPath));
+app.use(express.static("dist"));
 app.get("*", function (req, res) {
-  res.sendFile(staticDirPath + "/index.html");
+  res.sendFile(staticDirPath);
 });
 
 app.listen(port, () => {
