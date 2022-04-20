1840e15ff6480f0877e91f55b551d159799ed3cd
Adrian Villegas <manbassman1996@gmail.com>
2022-04-20 10:57:12 -0300

    prueba-deploy-vercel

--- back-src/index.ts
+++ back-src/index.ts
@@ -3,23 +3,23 @@ import * as path from "path";
 import * as cors from "cors";
 import { updateProfile, getProfile } from "./controllers/users-controller";
 import { User } from "./models/user";
-const staticDirPath = path.resolve(__dirname, "../dist/index.html");
-const port = process.env.PORT || 3004;
-
 const app = express();
+app.use(express.static("dist"));
 app.use(
   express.json({
     limit: "50mb",
   })
 );
 app.use(cors());
+const staticDirPath = path.resolve("", "dist/index.html");
+const port = process.env.PORT || 3004;
+
 app.get("/env", (req, res) => {
   res.json({
     message: process.env.NODE_ENV,
   });
 });
 
-app.use(express.static("dist"));
 app.get("*", function (req, res) {
   res.sendFile(staticDirPath);
 });
